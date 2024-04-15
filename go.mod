d26ef25aa7f71e83bc314967cd94111e283366f0
Madina <jumaniyozovamadina71@gmail.com>
2024-04-15 10:57:17 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/unknown3472/test_repo
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import(
+	"fmt"
+	"github.com/unknown3472/test_repo/greeting"
+)
+func main(){
+	fmt.Println(greeting.Greet())
+}
